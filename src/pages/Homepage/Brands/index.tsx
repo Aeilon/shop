51a07bd28823478e833347cb95c05bae80997dbb
Kamil Król <aeilon2332@gmail.com>
2021-04-07 15:32:23 +0200

    feat:Change brands logos
    Change brands logos color on hover

--- src/pages/Homepage/Brands/index.tsx
+++ src/pages/Homepage/Brands/index.tsx
@@ -30,8 +30,18 @@ const BrandsBox = styled.div`
   display: flex;
   width: auto;
   height: 12.6rem;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
+`;
+
+const Logo = styled.img`
+  filter: grayscale(100%);
+  transition: filter 2s ease-in-out;
+
+  &:hover {
+    filter: grayscale(0);
+    transition: filter 0.15s ease-in-out;
+  }
 `;
 
 export default () => {
@@ -41,12 +51,12 @@ export default () => {
         <Title>Our brands</Title>
       </TitleBox>
       <BrandsBox>
-        <img src={logitechLogo} alt="logitech" />
-        <img src={philipsLogo} alt="philips" />
-        <img src={linkedinLogo} alt="linkedin" />
-        <img src={amazonLogo} alt="amazon" />
-        <img src={facebookLogo} alt="facebook" />
-        <img src={googleLogo} alt="google" />
+        <Logo src={logitechLogo} alt="logitech" />
+        <Logo src={philipsLogo} alt="philips" />
+        <Logo src={linkedinLogo} alt="linkedin" />
+        <Logo src={amazonLogo} alt="amazon" />
+        <Logo src={facebookLogo} alt="facebook" />
+        <Logo src={googleLogo} alt="google" />
       </BrandsBox>
     </Main>
   );
